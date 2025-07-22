9ce697630d9935833d495ee16f4f93701fd95576
dpMelian <astrodaniel1234@gmail.com>
2025-07-22 23:16:18 +0100

    feat: load ROM file into memory, start game loop to load instructions

--- src/main.rs
+++ src/main.rs
@@ -1,13 +1,40 @@
 mod cpu;
+mod memory;
+
+use std::fs;
+
 use cpu::registers::Registers;
-use cpu::registers::RegisterPairs;
+use cpu::cpu::Cpu;
+
+use memory::memory::Memory;
+
+fn load_rom_file() -> Vec<u8> {
+  let rom = fs::read("roms/tetris.gb")
+    .expect("Should have been able to read the file");
+
+  if rom.is_empty() {
+    panic!("Could not load ROM");
+  }
+
+  rom
+}
 
 fn main() {
-  let mut cpu = cpu::cpu::CPU {
+  let rom = load_rom_file();
+
+  let mut cpu = Cpu {
     registers: Registers::new(),
   };
 
-  cpu.registers.set_pair( RegisterPairs::AF, 0x1901);
+  let mut memory = Memory::new();
 
-  dbg!(cpu.registers.get_pair(RegisterPairs::AF));
+  memory.load_rom_into_memory(&rom);
+
+  // Power up setup
+  cpu.registers.pc = 0x100;
+  cpu.registers.sp = 0xfffe;
+
+  while cpu.registers.pc <= 0x8000 {
+    cpu.run_instruction(&mut memory);
+  }
 }
