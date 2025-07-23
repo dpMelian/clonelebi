911a58c38db0cbdc79bc4b229847354e0c50b8b8
dpMelian <astrodaniel1234@gmail.com>
2025-07-23 23:54:38 +0100

    feat: implement some CPU instructions

--- src/cpu/cpu.rs
+++ src/cpu/cpu.rs
@@ -9,18 +9,21 @@ pub struct Cpu {
 impl Cpu {
   pub fn run_instruction(&mut self, memory: &mut Memory) {
     let opcode = memory.read(self.registers.pc);
-    self.registers.pc += 1;
 
     match opcode {
       0x00 => self.nop(),
       0x21 => self.ld_hl_nn(memory),
+      0x60 => self.ld_h_b(),
       0x7F => self.ld_a_a(),
       0xAF => self.xor_a_a(),
       0xC3 => self.jp_nn(memory),
-      0xDF => self.rst_18(),
-      0xFF => self.rst_38(),
+      0xDF => self.rst_18(memory),
+      0xFF => self.rst_38(memory),
+      0x2F => self.cpl(),
       _ => panic!("Instruction not yet implemented. Opcode: {:#X}", opcode)
     }
+
+    self.registers.pc += 1;
   }
 
   pub fn ld_a_a(&mut self) {
@@ -50,13 +53,36 @@ impl Cpu {
     self.registers.set_pair(RegisterPairs::HL, ((high as u16) >> 8) | low as u16);
   }
 
-  pub fn rst_18(&mut self) {
-    // todo!("push return address to stack");
+  pub fn rst_18(&mut self, memory: &mut Memory) {
+    let split_u8_values = self.registers.pc.to_le_bytes();
+
+    self.registers.sp -= 0x1;
+    memory.write(self.registers.sp, split_u8_values[0]);
+
+    self.registers.sp -= 0x1;
+    memory.write(self.registers.sp, split_u8_values[1]);
+    
     self.registers.pc = 0x0018;
   }
 
-  pub fn rst_38(&mut self) {
-    // todo!("push return address to stack");
+  pub fn rst_38(&mut self, memory: &mut Memory) {
+    let split_u8_values = self.registers.pc.to_le_bytes();
+
+    self.registers.sp -= 0x1;
+    memory.write(self.registers.sp, split_u8_values[0]);
+
+    self.registers.sp -= 0x1;
+    memory.write(self.registers.sp, split_u8_values[1]);
+    
     self.registers.pc = 0x0038;
+  }
+
+  pub fn ld_h_b(&mut self) {
+    self.registers.h = self.registers.b;
+  }
+
+  pub fn cpl(&mut self) {
+    print!("{:02X}", !(self.registers.a));
+    self.registers.a = !self.registers.a;
   }
 }
