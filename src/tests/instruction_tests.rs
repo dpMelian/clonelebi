fca40fff357797b62bb22eaf77511dfc23337ac6
dpMelian <astrodaniel1234@gmail.com>
2025-07-24 22:57:22 +0100

    feat: add inc test

--- src/tests/instruction_tests.rs
+++ src/tests/instruction_tests.rs
@@ -1,11 +1,14 @@
+use cpu::instructions::Optable;
 use cpu::registers::Registers;
 use cpu::cpu::Cpu;
 use memory::memory::Memory;
+
 
 #[test]
 fn test_cpl() {
   let mut cpu = Cpu {
     registers: Registers::new(),
+    optable: Optable::new(),
   };
 
   let mut memory = Memory::new();
@@ -17,4 +20,22 @@ fn test_cpl() {
   cpu.run_instruction(&mut memory);
 
   assert_eq!(cpu.registers.a, 0xEC);
+}
+
+#[test]
+fn test_inc() {
+  let mut cpu = Cpu {
+    registers: Registers::new(),
+    optable: Optable::new(),
+  };
+
+  let mut memory = Memory::new();
+  memory.write(0x0000, 0x3C);
+
+  cpu.registers.a = 0x13;
+  cpu.registers.pc = 0x00;
+
+  cpu.run_instruction(&mut memory);
+
+  assert_eq!(cpu.registers.a, 0x14);
 }
