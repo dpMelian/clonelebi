f05d5bada88e8b24f374096dd58180d6b10081f9
dpMelian <astrodaniel1234@gmail.com>
2025-07-25 13:29:40 +0100

    feat: make use of optable, parametrize functions

--- src/cpu/cpu.rs
+++ src/cpu/cpu.rs
@@ -1,39 +1,54 @@
+use cpu::instructions::Instruction;
+use cpu::instructions::Optable;
+use cpu::instructions::RstAddress;
 use cpu::registers::Registers;
-use cpu::registers::RegisterPairs;
+use cpu::registers::RegisterU8;
+use cpu::registers::Target;
 use memory::memory::Memory;
 
 pub struct Cpu {
   pub registers: Registers,
+  pub optable: Optable
 }
 
 impl Cpu {
+  pub fn new() -> Self {
+    Self {
+      registers: Registers::new(),
+      optable: Optable::new(),
+    }
+  }
+
   pub fn run_instruction(&mut self, memory: &mut Memory) {
     let opcode = memory.read(self.registers.pc);
 
-    match opcode {
-      0x00 => self.nop(),
-      0x21 => self.ld_hl_nn(memory),
-      0x60 => self.ld_h_b(),
-      0x7F => self.ld_a_a(),
-      0xAF => self.xor_a_a(),
-      0xC3 => self.jp_nn(memory),
-      0xDF => self.rst_18(memory),
-      0xFF => self.rst_38(memory),
-      0x2F => self.cpl(),
-      _ => panic!("Instruction not yet implemented. Opcode: {:#X}", opcode)
+    match &self.optable.optable[opcode as usize] {
+      Instruction::Nop => Self::nop(self, memory),
+      Instruction::Unimplemented => Self::unimplemented_instruction(self, memory),
+      Instruction::LdR1R2(r1, r2) => Self::ld_r1_r2(self, memory, *r1, *r2),
+      Instruction::JpNN => Self::jp_nn(self, memory),
+      Instruction::Cpl => Self::cpl(self, memory),
+      Instruction::Rst(jump_address) => Self::rst_n(self, memory, *jump_address),
+      Instruction::Inc(r1) => Self::inc_n(self, memory, *r1),
+      Instruction::Xor(r1) => Self::xor_n(self, memory, *r1),
+      Instruction::LdNNn(n) => Self::ld_n_nn(self, memory, *n),
     }
 
     self.registers.pc += 1;
   }
 
-  pub fn ld_a_a(&mut self) {
-    self.registers.a = self.registers.a;
+  fn unimplemented_instruction(&mut self, memory: &mut Memory) {
+    panic!("Instruction not yet implemented. Opcode: 0x{:02X}. PC: 0x{:02X}", memory.read(self.registers.pc), self.registers.pc);
   }
 
-  pub fn nop(&self) {
+  fn nop(&mut self, _memory: &mut Memory) {
   }
 
-  pub fn jp_nn(&mut self, memory: &mut Memory) {
+  fn ld_r1_r2(&mut self, _memory: &mut Memory, r1: RegisterU8, r2: RegisterU8) {
+    self.registers[r1] = self.registers[r2];
+  }
+
+  fn jp_nn(&mut self, memory: &mut Memory) {
     let low: u8 = memory.read(self.registers.pc);
     self.registers.pc += 1;
     let high: u8 = memory.read(self.registers.pc);
@@ -41,48 +56,41 @@ impl Cpu {
     self.registers.pc = ((high as u16) << 8) | (low as u16);
   }
 
-  pub fn xor_a_a(&mut self) {
-    self.registers.a = self.registers.a ^ self.registers.a;
+  fn cpl(&mut self, _memory: &mut Memory) {
+    self.registers.a = !self.registers.a;
   }
 
-  pub fn ld_hl_nn(&mut self, memory: &mut Memory) {
+  fn rst_n(&mut self, memory: &mut Memory, jump_address: RstAddress) {
+    let split_u8_values = self.registers.pc.to_le_bytes();
+
+    self.registers.sp -= 0x1;
+    memory.write(self.registers.sp, split_u8_values[0]);
+
+    self.registers.sp -= 0x1;
+    memory.write(self.registers.sp, split_u8_values[1]);
+    
+    self.registers.pc = jump_address as u16;
+  }
+
+  fn inc_n(&mut self, _memory: &mut Memory, r1: RegisterU8) {
+    self.registers[r1] += 1;
+  }
+
+  fn xor_n(&mut self, _memory: &mut Memory, r1: RegisterU8) {
+    self.registers[r1] = self.registers[r1] ^ self.registers[r1];
+  }
+
+  fn ld_n_nn(&mut self, memory: &mut Memory, n: Target) {
     let low: u8 = memory.read(self.registers.pc);
     self.registers.pc += 1;
     let high: u8 = memory.read(self.registers.pc);
 
-    self.registers.set_pair(RegisterPairs::HL, ((high as u16) >> 8) | low as u16);
-  }
+    if let Target::Single(_register) = n {
+      self.registers.sp = ((high as u16) >> 8) | (low as u16);
+    }
 
-  pub fn rst_18(&mut self, memory: &mut Memory) {
-    let split_u8_values = self.registers.pc.to_le_bytes();
-
-    self.registers.sp -= 0x1;
-    memory.write(self.registers.sp, split_u8_values[0]);
-
-    self.registers.sp -= 0x1;
-    memory.write(self.registers.sp, split_u8_values[1]);
-    
-    self.registers.pc = 0x0018;
-  }
-
-  pub fn rst_38(&mut self, memory: &mut Memory) {
-    let split_u8_values = self.registers.pc.to_le_bytes();
-
-    self.registers.sp -= 0x1;
-    memory.write(self.registers.sp, split_u8_values[0]);
-
-    self.registers.sp -= 0x1;
-    memory.write(self.registers.sp, split_u8_values[1]);
-    
-    self.registers.pc = 0x0038;
-  }
-
-  pub fn ld_h_b(&mut self) {
-    self.registers.h = self.registers.b;
-  }
-
-  pub fn cpl(&mut self) {
-    print!("{:02X}", !(self.registers.a));
-    self.registers.a = !self.registers.a;
+    if let Target::Pair(register_pair) = n {
+      self.registers.set_pair(register_pair, ((high as u16) >> 8) | low as u16);
+    }
   }
 }
--- src/cpu/registers.rs
+++ src/cpu/registers.rs
@@ -1,3 +1,23 @@
+use std::ops::{Index, IndexMut};
+
+#[derive(Copy, Clone, Debug, PartialEq, Eq)]
+pub enum RegisterU8 {
+  A,
+  B,
+  C,
+  D,
+  E,
+  F,
+  H,
+  L,
+}
+
+#[derive(Copy, Clone, Debug, PartialEq, Eq)]
+pub enum RegisterU16 {
+  SP,
+  PC,
+}
+
 pub struct Registers {
   pub a: u8, // Accumulator
   pub b: u8,
@@ -11,11 +31,70 @@ pub struct Registers {
   pub pc: u16, // Program Counter/Pointer
 }
 
-pub enum RegisterPairs {
+impl Index<RegisterU8> for Registers {
+  type Output = u8;
+
+  fn index(&self, register: RegisterU8) -> &Self::Output {
+    match register {
+      RegisterU8::A => &self.a,
+      RegisterU8::B => &self.b,
+      RegisterU8::C => &self.c,
+      RegisterU8::D => &self.d,
+      RegisterU8::E => &self.e,
+      RegisterU8::F => &self.f,
+      RegisterU8::H => &self.h,
+      RegisterU8::L => &self.l,
+    }
+  }
+}
+
+impl Index<RegisterU16> for Registers {
+  type Output = u16;
+
+  fn index(&self, register: RegisterU16) -> &Self::Output {
+    match register {
+      RegisterU16::SP => &self.sp,
+      RegisterU16::PC => &self.pc
+    }
+  }
+}
+
+impl IndexMut<RegisterU8> for Registers {
+  fn index_mut(&mut self, register: RegisterU8) -> &mut Self::Output {
+    match register {
+      RegisterU8::A => &mut self.a,
+      RegisterU8::B => &mut self.b,
+      RegisterU8::C => &mut self.c,
+      RegisterU8::D => &mut self.d,
+      RegisterU8::E => &mut self.e,
+      RegisterU8::F => &mut self.f,
+      RegisterU8::H => &mut self.h,
+      RegisterU8::L => &mut self.l,
+    }
+  }
+}
+
+impl IndexMut<RegisterU16> for Registers {
+  fn index_mut(&mut self, register: RegisterU16) -> &mut Self::Output {
+    match register {
+      RegisterU16::SP => &mut self.sp,
+      RegisterU16::PC => &mut self.pc
+    }
+  }
+}
+
+#[derive(Copy, Clone, Debug, PartialEq, Eq)]
+pub enum RegisterPair {
   AF,
   BC,
   DE,
   HL,
+}
+
+#[derive(Copy, Clone, Debug, PartialEq, Eq)]
+pub enum Target {
+  Pair(RegisterPair),
+  Single(RegisterU16)
 }
 
 impl Registers {
@@ -23,32 +102,32 @@ impl Registers {
     Self { a: 0, b: 0, c: 0, d: 0, e: 0, f: 0, h: 0, l: 0, sp: 0, pc: 0 }
   }
 
-  pub fn get_pair(&self, case: RegisterPairs) -> u16 {
+  pub fn get_pair(&self, case: RegisterPair) -> u16 {
     match case {
-      RegisterPairs::AF => (self.a as u16 * 256) + self.f as u16,
-      RegisterPairs::BC => (self.b as u16 * 256) + self.c as u16,
-      RegisterPairs::DE => (self.d as u16 * 256) + self.e as u16,
-      RegisterPairs::HL => (self.h as u16 * 256) + self.l as u16,
+      RegisterPair::AF => (self.a as u16 * 256) + self.f as u16,
+      RegisterPair::BC => (self.b as u16 * 256) + self.c as u16,
+      RegisterPair::DE => (self.d as u16 * 256) + self.e as u16,
+      RegisterPair::HL => (self.h as u16 * 256) + self.l as u16,
     }
   }
 
-  pub fn set_pair(&mut self, case: RegisterPairs, value: u16) {
+  pub fn set_pair(&mut self, case: RegisterPair, value: u16) {
     let split_u8_values = value.to_be_bytes();
     
     match case {
-      RegisterPairs::AF => {
+      RegisterPair::AF => {
         self.a = split_u8_values[0];
         self.f = split_u8_values[1];
       },
-      RegisterPairs::BC => {
+      RegisterPair::BC => {
         self.b = split_u8_values[0];
         self.c = split_u8_values[1];
       },
-      RegisterPairs::DE => {
+      RegisterPair::DE => {
         self.d = split_u8_values[0];
         self.e = split_u8_values[1];
       },
-      RegisterPairs::HL => {
+      RegisterPair::HL => {
         self.h = split_u8_values[0];
         self.l = split_u8_values[1];
       },
